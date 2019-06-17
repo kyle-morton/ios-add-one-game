b43b857311a933d156e41cd8dfbb85ac28e8103d
Kyle Morton <mkmorton91@gmail.com>
2019-06-17 13:53:58 -0500

    working through tutorial

---
+++ Add1/Add1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Add1
+//
+//  Created by Kyle Morton on 6/17/19.
+//  Copyright © 2019 LearnAppMaking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
