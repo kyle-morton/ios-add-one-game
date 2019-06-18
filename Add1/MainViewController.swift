d0be8bc8e88ba9d797331fdd8d1851c0c3a93679
Kyle Morton <mkmorton91@gmail.com>
2019-06-18 13:06:54 -0500

    added game logic, now working through emulator (part 2 of tutorial)

--- Add1/MainViewController.swift
+++ Add1/MainViewController.swift
@@ -10,21 +10,69 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var numbersLabel:UILabel?
+    @IBOutlet weak var scoreLabel:UILabel?
+    @IBOutlet weak var inputField:UITextField?
+    
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setRandomNumberLabel()
+        updateScoreLabel()
 
-        // Do any additional setup after loading the view.
+        inputField?.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func updateScoreLabel()
+    {
+        scoreLabel?.text = "\(score)"
     }
-    */
+    
+    func setRandomNumberLabel() {
+        numbersLabel?.text = generateRandomString()
+    }
+    
+    @objc func textFieldDidChange(textField: UITextField) {
+        
+        if (inputField?.text?.count ?? 0 < 4) {
+            return;
+        }
+        
+        if let numbers_text = numbersLabel?.text,
+           let input_text = inputField?.text,
+           let numbers = Int(numbers_text),
+           let input = Int(input_text) {
+           
+            print("Comparing: \(input_text) minus \(numbers_text) == \(input - numbers)")
+            
+            if (input - numbers == 1111){
+                print("Correct!")
+                score += 1
+            } else {
+                print("Incorrect!")
+                score -= 1
+                
+            }
+            
+        }
+        
+        setRandomNumberLabel()
+        updateScoreLabel()
+    }
+    
+    func generateRandomString() -> String
+    {
+        var result:String = ""
+        
+        for _ in 1...4 {
+            let digit = Int(arc4random_uniform(8) + 1)
+    
+            result += "\(digit)"
+        }
+    
+        return result
+    }
 
 }
